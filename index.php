7638ad9a26995554d90137371d7b0b5a7d0497bf
Ayman <contacto.ayman@gmail.com>
2023-10-23 14:08:44 +0200

    SEGUIR

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
 		
 		function extract_bolas_bombo(){
 			global $cantidad_jugadores;
+			$cantar_bingo = false;
 			
 			// 5# Altero aleatoriamente la posición de los numeros
 			$bombo = range(1,60);
@@ -55,6 +56,10 @@
 				unset($bombo[0]);
 				$bombo = array_values($bombo);
 				
+				// Muestra las bolas con "algo" de estética
+				echo "<img src='images/".$numero.".png'/>";
+				
+				
 				// 6# Saco un numero del bombo. Se queda un hueco, asigno cada uno de los valores al mismo array para
 				// llenar huecos
 				
@@ -64,12 +69,18 @@
 						
 						// 7# Cada numero que saco, llevo a comparar a las cartulinas del jugadorX.
 						// Si retorna 1, dicho jugador dió BINGO
+						$cantar_bingo = true;
 						echo $nombre_jugador." CANTÓ BINGOOOOO !!!!!!!";
 						break 2; // Me salgo de 2 loops, del FOR y del DO_WHILE
 					}
 				}
-				
 			}while(count($bombo) > 0);
+			
+			
+			
+			if(!$cantar_bingo){
+				echo "Nadie cantó bingo ;(";
+			}
 		}
 		
 		function crear_jugadores_y_cartones(){
@@ -84,6 +95,7 @@
 						
 						// 3# Añadimos por cada jugador, 3 cartones y cada uno tendrá 15 numeros
 						$numero_aleatorio = rand(0,60);
+						// SEGUIR -> Se puede repetir X numero en diferentes cartones, pero no en el mismo
 						$jugadores["jugador".$jugador][$carton][$numero] = $numero_aleatorio;
 						
 					}
